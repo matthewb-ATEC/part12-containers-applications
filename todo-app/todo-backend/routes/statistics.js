c787a6f7ac73660e468f21dc39ad67c27f35d89b
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-13 15:47:28 -0500

    12.16

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const redis = require('redis-promisify')
 
-const client = redis.createClient()
+const client = redis.createClient({
+  host: 'redis',
+  port: 6379,
+})
 
 router.get('/', async (_, res) => {
   const value = await client.getAsync('added_todos')
